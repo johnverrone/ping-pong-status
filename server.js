f06c9fa3b8b5607a89c73212b96bf5bc01f6d7ee
John Verrone <john_verrone@homedepot.com>
2017-10-27 12:59:59 -0400

    using port variable for PCF

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log('Listening on port :3000');
+var port = process.env.PORT || 3000;
+
+http.listen(port, function() {
+  console.log('Listening on port :' + port);
 });
